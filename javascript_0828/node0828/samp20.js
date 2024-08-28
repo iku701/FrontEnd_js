a4e071044d527986c777e00a31b1a8c43db4697e
iku701 <iku701@naver.com>
2024-08-28 18:30:28 +0900

    0828secondCommit

---
+++ javascript_0828/node0828/samp20.js
@@ -0,0 +1,7 @@
+console.log(0);
+
+setTimeout(function(){
+    console.log('hello');
+},0);
+
+console.log(1);
---
+++ javascript_0828/node0828/samp22.js
@@ -0,0 +1,17 @@
+function workP(sec) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(new Date().toISOString());
+        }, sec * 1000);
+    });
+}
+
+workP(1).then((result) => {
+    console.log('첫번째 작업', result);
+    return workP(1);
+}).then((result) => {
+    console.log('두번째 작업', result);
+    return workP(1);
+}).then((result) => {
+    console.log('세번째 작업', result);
+});
